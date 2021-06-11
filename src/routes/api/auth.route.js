9059a89f63e6067fe725c3ced7bd28a62a25aed0
mathieugery <mathieu.gery@epitech.eu>
2021-06-11 15:13:40 +0200

    [+] Can create new users only by admin

--- src/routes/api/auth.route.js
+++ src/routes/api/auth.route.js
@@ -7,11 +7,7 @@ const validator = require('express-validation')
 const { create } = require('../../validations/user.validation')
 const auth = require('../../middlewares/authorization')
 
-router.post('/register', validator(create), authController.register)
+router.post('/register', auth(['admin']), validator(create), authController.register)
 router.post('/login', authController.login)
-router.get('/confirm', authController.confirm)
-router.post('/recover_password', authController.recoverPassword)
-router.post('/recover_password_final', authController.recoverPasswordFinal)
-router.get('/list', auth(), authController.list)
 
 module.exports = router
