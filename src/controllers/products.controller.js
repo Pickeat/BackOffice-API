73518367e60144c2bb9ad8ae7fd1cb2f3046d468
mathieugery <mathieu.gery@epitech.eu>
2021-06-14 14:42:24 +0200

    [+] Number of products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,7 +5,7 @@ const Products = require('../models/products.model')
 exports.getProducts = async (req, res, next) => {
     try {
         const products = await Products.listProducts()
-        return res.json({products: products})
+        return res.json({products_number: products.length, products: products})
     } catch (error) {
         next(error)
     }
